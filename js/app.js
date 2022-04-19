562035ac22659e9616f17a65c2058f085e9e9f0d
Sherita Manning <smanningsn@gmail.com>
2022-04-18 20:04:14 -0700

    Created js variables

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const qwery = document.getElementById('qwery');
+const phrase = document.getElementById('phrase');
+const btnReset = document.getElementsByClassName('btn__reset');
+const missed = 0;
